54045d41740fb98dd0a0d56b7f9e6a5ec8958580
Nguyen Kim Dien <trietnk01@gmail.com>
2025-11-25 18:23:37 +0700

    Add slider mask

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -456,8 +456,9 @@ const Home = () => {
           <div className={clsx(["relative", "w-full", "h-full", "z-1", styles.banners])}>
             {banners.map((item: IBanner, idx: number) => {
               return (
-                <div key={`banner-slide-${idx}`} style={{ zIndex: item.active ? 99 : idx }} className={clsx(["absolute", "w-full", "h-full", "top-0", "left-0", item.active && directionSlider === "prev" && styles.activeSliderPrev, item.active && directionSlider === "next" && styles.activeSliderNext])}>
+                <div key={`banner-slide-${idx}`} style={{ zIndex: item.active ? 99 : idx }} className={clsx(["absolute", "w-full", "h-full", "top-0", "left-0", styles.slide, item.active === true ? "opacity-100" : "opacity-0", item.active && directionSlider === "prev" && styles.activeSliderPrev, item.active && directionSlider === "next" && styles.activeSliderNext])}>
                   <Image alt="Website" width={1700} height={717} src={`/${item.img}`} className={clsx(["w-full", "h-full"])} />
+                  <div className={clsx(["absolute", "top-0", "left-0", "w-full", "h-full", "shadow-md", item.active && styles.mask])}></div>
                 </div>
               );
             })}
